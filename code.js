841c0b386a4a0121c222d0706b8e35875b19401f
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-01-30 18:15:56 +0000

    .

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.lenghth == 0) {
+        return 0;
+    }
+    if (a.length == 1) {
+        return a[0];
+    }
+
+    const oneThird = Math.floor(a.length / 3);
+    const twoThirds = 2 * oneThird;
+
+    const leftSide = a.slice(0, oneThird);
+    const middle = a.slice(third, twoThirds);
+    const rightSide = a.slice(twoThirds);
+
+    return divideAndConquerSum(leftSide) + divideAndConquerSum(middle) + divideAndConquerSum(rightSide);
 }
