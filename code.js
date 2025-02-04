1469de31665bbf0b530e7e6018a3ff0167304fa0
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-02-04 10:58:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 function divideAndConquerSum(a) {
-    if (a.lenghth == 0) {
+    if (a.length === 0) {
         return 0;
     }
-    if (a.length == 1) {
+    if (a.length === 1) {
         return a[0];
     }
 
@@ -10,7 +10,7 @@ function divideAndConquerSum(a) {
     const twoThirds = 2 * oneThird;
 
     const leftSide = a.slice(0, oneThird);
-    const middle = a.slice(third, twoThirds);
+    const middle = a.slice(oneThird, twoThirds);
     const rightSide = a.slice(twoThirds);
 
     return divideAndConquerSum(leftSide) + divideAndConquerSum(middle) + divideAndConquerSum(rightSide);
